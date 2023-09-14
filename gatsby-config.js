2f90a4ee0184182b364b353ce99a8f9191614816
mnpsnuwan <nuwanp124@gmail.com>
2023-09-13 19:32:24 -0700

    IM:DEV:Personal portfolio - Update site url

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
     title: 'Nuwan Samarasinghe',
     description:
       'Nuwan Samarasinghe is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://nuwan.github.io', // No trailing slash allowed!
+    siteUrl: 'https://mnpsnuwan.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@NuwanPrabhath',
   },
