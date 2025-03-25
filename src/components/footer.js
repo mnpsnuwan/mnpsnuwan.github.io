ec0b4f9bce952ca0ad6b8a5f21b5c55efce62832
mnpsnuwan <nuwanp124@gmail.com>
2025-03-24 18:02:15 -0700

    IM:DEV:Portfolio - Update Footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -91,7 +91,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <div>
-          Copyright © 2024
+          Copyright © 2023-2025
           <a href="https://mnpsnuwan.github.io">mnpsnuwan.github.io</a>Powered By
           <a href="https://imatrixlabs.com">IMATRIXLABS (PVT) LTD.</a>
         </div>
